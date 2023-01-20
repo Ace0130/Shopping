0f75a25b2687dd633704add936f849f6115a8d09
jaewonE <importjaewonE@gmail.com>
2023-01-20 10:38:22 +0900

    code clean2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import { BrowserRouter } from 'react-router-dom';
+import Test from './test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    {/* <App /> */}
+    <Test />
   </BrowserRouter>
 );
---
+++ src/test.js
@@ -0,0 +1,10 @@
+import MainProduct from './components/mainProduct';
+import MainProductList from './ui/main-product-list';
+
+export default function Test() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <MainProductList />
+    </div>
+  );
+}
